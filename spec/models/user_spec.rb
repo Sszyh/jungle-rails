ec9755d1c818c34487b169440c896007387c7c33
Sszyh <zhuyer@outlook.com>
2023-01-19 22:38:32 -0800

    modify test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,78 +1,80 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'password_confirmation' do
-    it 'should match' do
-      @user = User.new({
-        name: 'Ss',
-        email: 'ss@gmail.com',
-        password: "123",
-        password_confirmation:"123"
-      })
-      expect(@user).to be_valid
+  describe 'Validations' do
+    describe 'password_confirmation' do
+      it 'should match' do
+        @user = User.new({
+          name: 'Ss',
+          email: 'ss@gmail.com',
+          password: "123",
+          password_confirmation:"123"
+        })
+        expect(@user).to be_valid
+      end
+
+      it 'should exist when creating the model' do
+        @user = User.new({
+          name: 'Ss',
+          email: 'ss@gmail.com',
+          password: "123",
+          password_confirmation:"123"
+        })
+        expect(@user.password).to be_present
+      end
     end
 
-    it 'should exist when creating the model' do
-      @user = User.new({
-        name: 'Ss',
-        email: 'ss@gmail.com',
-        password: "123",
-        password_confirmation:"123"
-      })
-      expect(@user.password).to be_present
-    end
-  end
-
-  describe 'Email' do
-    it 'should be unique' do
-      @user = User.new({
-        name: 'Ss',
-        email: 'ss@gmail.com',
-        password: "123",
-        password_confirmation:"123"
-      })
-      @user.save!
-      @user2 = User.new({
-        name: 'Qq',
-        email: 'SS@gmail.com',
-        password: "123",
-        password_confirmation:"123"
-      })
-      expect {@user2.save!}.to raise_error(ActiveRecord::RecordInvalid,'Validation failed: Email has already been taken')
-    end
-  end
-
-  describe 'field required' do
-    it 'should require Email, name' do
-      @user = User.new({
-        
-        email: 'aa@gmail.com',
-        password: "123",
-        password_confirmation:"123"
-      })
-      expect(@user).to_not be_valid
+    describe 'Email' do
+      it 'should be unique' do
+        @user = User.new({
+          name: 'Ss',
+          email: 'ss@gmail.com',
+          password: "123",
+          password_confirmation:"123"
+        })
+        @user.save!
+        @user2 = User.new({
+          name: 'Qq',
+          email: 'SS@gmail.com',
+          password: "123",
+          password_confirmation:"123"
+        })
+        expect {@user2.save!}.to raise_error(ActiveRecord::RecordInvalid,'Validation failed: Email has already been taken')
+      end
     end
 
-    it 'should require Email, name' do
-      @user = User.new({
-        name: 'Ss',
-        
-        password: "123",
-        password_confirmation:"123"
-      })
-      expect(@user).to_not be_valid
-    end
-  end
+    describe 'field required' do
+      it 'should require Email, name' do
+        @user = User.new({
+          
+          email: 'aa@gmail.com',
+          password: "123",
+          password_confirmation:"123"
+        })
+        expect(@user).to_not be_valid
+      end
 
-  describe 'password length' do
-    it 'should more than 6' do
-      @user = User.new({
-        name: 'rr',
-        email: 'rr@gmail.com',
-        password: "12",
-        password_confirmation:"12"
-      })
-      expect{@user.save!}.to raise_error(ActiveRecord::RecordInvalid,'Validation failed: Password is too short (minimum is 3 characters)')
+      it 'should require Email, name' do
+        @user = User.new({
+          name: 'Ss',
+          
+          password: "123",
+          password_confirmation:"123"
+        })
+        expect(@user).to_not be_valid
+      end
+    end
+
+    describe 'password length' do
+      it 'should more than 6' do
+        @user = User.new({
+          name: 'rr',
+          email: 'rr@gmail.com',
+          password: "12",
+          password_confirmation:"12"
+        })
+        expect{@user.save!}.to raise_error(ActiveRecord::RecordInvalid,'Validation failed: Password is too short (minimum is 3 characters)')
+      end
     end
   end
 
